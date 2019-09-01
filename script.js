54dc6e31017d0da17d687ffe6bf32449aeed20ea
Max Sazhin <jooher@gmail.com>
2019-09-01 13:04:04 +0300

    balki

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ const	form	= document.getElementById("form"),
 	doska	= w=> w<100?40 : w<150?70 : w<200?100 : w,
 	
 	height	= form.elements["height"],
+	
+	K		= 2,
 		
 	calc	= ()=>{
 		const	$	= input(),
 			p	= document.querySelector("input[name='profile']:checked").value,
 			//p	= $.profile,
-			doski	= doska($.width)*Math.round(p/10) + doska($.height)*(p%10),
-			brush	= $.brush ? doski*.5 : 0,
-			kraska	= $.paint ? doski*.4 : 0,
-			meter	= doski + brush + kraska,
+			
+			doski	= doska($.width)*[0,1,1,1,2][p] + doska($.height)*[0,0,1,2,2][p],
+			brush	= doski*[0,1.5,2][$.brush||0],
+			paint	= doski*[0,2,3,5][$.paint||0],
+			meter	= doski + brush + paint,
 			balka	= meter * $.length *.001,
 			summa	= balka * $.qty,
 			skidka	= summa>1e5?25 : summa>5e4?15 :  summa>1e4?10 : 0,
-			itogo	= summa * (1-(.01*skidka));
+			itogo	= Math.round(summa * (1-(.01*skidka)));
 			
 		height.disabled = p=='10';
 			
